6e46013ad6cd90a95f1b6a175cfeda8a7c2bbffa
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-26 17:31:13 -0400

    renames

--- src/main/java/ex24/App.java
+++ src/main/java/ex24/App.java
@@ -1,13 +1,13 @@
 /*
- *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
  *  Copyright 2021 Michael Vuolo
  */
 
-package edu.ucf;
+package ex24;
 
 import java.util.Scanner;
 
-public class exercise {
+public class App {
     public static void main(String[] args) {
         // prompt for name
         System.out.print("What is your name? ");
--- src/test/java/ex24/AppTest.java
+++ src/test/java/ex24/AppTest.java
@@ -1,14 +1,14 @@
 /*
- *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
  *  Copyright 2021 Michael Vuolo
  */
 
-package edu.ucf;
+package ex24;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class exerciseTest {
+public class AppTest {
     @Test
     public void returnTrue()
     {
