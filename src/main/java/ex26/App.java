48603d4dfe2946edc8e6f0e15991ab3fa489e838
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-26 19:18:22 -0400

    done w/ ex26

--- src/main/java/ex26/App.java
+++ src/main/java/ex26/App.java
@@ -5,6 +5,8 @@
 
 package ex26;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class App {
@@ -12,11 +14,28 @@ public class App {
         // create new scanner
         Scanner input = new Scanner(System.in);
 
-//        // prompt for password
-//        System.out.print("Enter a password: ");
-//        String password = input.nextLine();
-//
-//        // output password strength
-//        System.out.print("The password '" + password + "' is a " + passwordStrengthStr + " password.");
+        // prompt for credit card information (and convert to two decimal places - nearest cent)
+        System.out.print("What is your balance? ");
+        float b = new BigDecimal(input.nextFloat()).setScale(2, RoundingMode.HALF_UP).floatValue();
+        System.out.print("What is the APR on the card (as a percent)? ");
+        float apr = new BigDecimal(input.nextFloat()).setScale(2, RoundingMode.HALF_UP).floatValue();
+        System.out.print("What is the monthly payment you can make? ");
+        float p = new BigDecimal(input.nextFloat()).setScale(2, RoundingMode.HALF_UP).floatValue();
+
+        // calculate months to pay off card
+        int numMonths = new PaymentCalculator().calculateMonthsUntilPaidOff(b, apr, p);
+
+        // output months to pay off card
+        System.out.print("It will take you " + numMonths + " months to pay off this card.");
+    }
+}
+
+class PaymentCalculator {
+    public int calculateMonthsUntilPaidOff(float b, float apr, float p) {
+        // calculate daily rate
+        float i = apr / 100 / 365;
+
+        // return number of months to pay off
+        return (int) Math.ceil((-1 / 30.0) * Math.log(1 + b / p * (1 - Math.pow(1 + i, 30))) / Math.log(1 + i));
     }
 }
