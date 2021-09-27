a482b8d4ba1b73761a9e6a5e90615c94a9100756
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-27 17:20:31 -0400

    done w ex 35

--- src/main/java/ex35/App.java
+++ src/main/java/ex35/App.java
@@ -5,6 +5,8 @@
 
 package ex35;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
@@ -12,6 +14,25 @@ public class App {
         // create new scanner
         Scanner input = new Scanner(System.in);
 
+        // initialize variables
+        String curName;
+        ArrayList<String> names = new ArrayList<String>();
+        Random rand = new Random();
 
+        // loop until done entering names
+        do {
+            // prompt for name
+            System.out.print("Enter a name: ");
+            curName = input.nextLine();
+
+            // add name to array
+            names.add(curName);
+        } while (!curName.isEmpty()); // don't allow blank entry
+
+        // remove blank entry
+        names.remove(names.size() - 1);
+
+        // display random winner
+        System.out.println("The winner is... " + names.get(rand.nextInt(names.size())) + ".");
     }
 }   
---
+++ src/test/java/ex36/AppTest.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Michael Vuolo
+ */
+
+package ex36;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void returnTrue()
+    {
+        assertTrue(true);
+    }
+}
